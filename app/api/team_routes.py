da2443c46078a86f2436b69a75943f524b952938
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-06-03 00:43:56 -0400

    Added a update teams backend route

--- app/api/team_routes.py
+++ app/api/team_routes.py
@@ -5,10 +5,13 @@ from .lists import team_words
 from datetime import datetime
 
 import random
-import datetime
 
-today = datetime.date.today()
-current_year = today.year
+# import datetime
+# today = datetime.date.today()
+# current_year = today.year
+
+timestamp = datetime.now()
+
 
 team_routes = Blueprint('teams', __name__)
 
@@ -43,3 +46,23 @@ def delete_team(id):
     db.session.commit()
 
     return {"team": team.to_dict()}
+
+@team_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_team(id):
+    # Updates an existing team with the given id if the user is the league's admin or the team owner
+    team = Team.query.get(id)
+    # if team == None:
+    #     return jsonify(error=["team_id is not valid."]), 401
+
+    if team.user_id != current_user.id:
+        return jsonify(error=["You don't have the permission to edit this team."]), 401
+
+    name = request.json.get('name')
+    # print(name)
+    team.name = name or team.name
+    team.updated_at = timestamp
+
+    db.session.commit()
+
+    return team.to_dict()
