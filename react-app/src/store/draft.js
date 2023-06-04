1c1634e7a011821e8fb756979b56fd1f0f02f6db
leongkevin <99384279+leongkevin@users.noreply.github.com>
2023-06-04 19:52:05 -0400

    Save progress for POST_DRAFT, not tested yet

--- react-app/src/store/draft.js
+++ react-app/src/store/draft.js
@@ -1,6 +1,6 @@
 // constant
 const LOAD_DRAFT = '/DRAFTS/LOAD_DRAFT';
-
+const POST_DRAFT = '/DRAFTS/POST_DRAFT';
 
 // action creators - define actions( objects with type/data )
 const loadDraft = (draft) => ({
@@ -8,6 +8,10 @@ const loadDraft = (draft) => ({
 	payload: draft,
 });
 
+const postDraft = () => ({
+	type: POST_DRAFT,
+	payload: draft,
+});
 
 // // thunk action creators - for asynchronous code, i.e fetch calls prior to dispatching action creators
 export const getDraft = () => async (dispatch) => {
@@ -16,7 +20,7 @@ export const getDraft = () => async (dispatch) => {
 	// console.log(teams)
 	try {
 		const response = await fetch('/api/drafts');
-		console.log(response)
+		console.log(response);
 		if (response.ok) {
 			const drafts = await response.json();
 			// console.log('getTeams.line 17', teams);
@@ -28,6 +32,25 @@ export const getDraft = () => async (dispatch) => {
 	}
 };
 
+export const draftPlayer = (data) => async (dispatch) => {
+	try {
+		const response = await fetch(`/api/drafts`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+
+		if (response.ok) {
+			const draft = await res.json();
+			dispatch(postDraft(draft));
+			return draft;
+		} else return res.json();
+	} catch (err) {
+		return err;
+	}
+};
 
 // reducer
 const initialState = {};
@@ -41,6 +64,11 @@ const draftReducer = (state = initialState, action) => {
 				// console.log('teamReducer', el);
 			});
 			return newState;
+		}
+
+		case POST_DRAFT: {
+			const newState = { ...state };
+			return { ...newState, [action.playload.id]: action.payload };
 		}
 
 		default: {
